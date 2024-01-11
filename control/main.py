62e9183c9b4a8569adf31aee0dabcf8b961b00e0
LuisUlloaC <luis.ulloa75360@gmail.com>
2024-01-11 13:11:13 -0500

    Can edit clients now

--- control/main.py
+++ control/main.py
@@ -13,7 +13,7 @@ from views.developer import DevDialog
 from .validators import CIValidator, InputsValidator
 
 class ClientListItem(QWidget):
-    view_changed = pyqtSignal(int)
+    view_changed = pyqtSignal(int, str)
     delete_client_signal = pyqtSignal(str)
 
     def __init__(self, client, assets_path):
@@ -44,18 +44,13 @@ class ClientListItem(QWidget):
     def get_media(self, file):
         media_path = os.getcwd()
         media_path = media_path.replace("\\", "/")
-        print('La ruta: '+ os.path.join(media_path, file))
         return os.path.join(media_path, file)
 
     def handle_view_changed(self):
-        self.view_changed.emit(2)
+        self.view_changed.emit(2, self.client['ci'])
 
     def handle_delete_client(self):
         self.delete_client_signal.emit(self.client['ci'])
-
-    def edit_client(self):
-        pass
-
     
 
 class Main(QMainWindow):
@@ -65,7 +60,9 @@ class Main(QMainWindow):
         super(Main, self).__init__()
         uic.loadUi(self.get_assets("mainWindow.ui"), self)
         self.setWindowTitle("GyManager")
+        self.editing_client_ci = 0
         self.creating_client_picture = None
+        self.creating_client_edit_picture = None
         self.error_ci.hide()
         self.error_nombre.hide()
         self.error_primer_apellido.hide()
@@ -80,16 +77,17 @@ class Main(QMainWindow):
         self.close_sidebar_button.clicked.connect(self.close)
         self.add_client_button.clicked.connect(self.goto_client_creation_form)
         self.picture.mousePressEvent = self.select_client_picture
+        self.picture_edit.mousePressEvent = self.select_client_edit_picture
         self.accept_button.clicked.connect(self.create_client)
         self.decline_button.clicked.connect(self.goto_clients_list)
         ci_string_validator = CIValidator(self.error_ci)
         nombre_number_validator = InputsValidator(self.error_nombre)
         primer_apellido_number_validator = InputsValidator(self.error_primer_apellido)
         segundo_apellido_number_validator = InputsValidator(self.error_segundo_apellido)
-        ci_edit_string_validator = CIValidator(self.error_ci)
-        nombre_edit_number_validator = InputsValidator(self.error_nombre)
-        primer_edit_apellido_number_validator = InputsValidator(self.error_primer_apellido)
-        segundo_edit_apellido_number_validator = InputsValidator(self.error_segundo_apellido)
+        ci_edit_string_validator = CIValidator(self.error_ci_edit)
+        nombre_edit_number_validator = InputsValidator(self.error_nombre_edit)
+        primer_edit_apellido_number_validator = InputsValidator(self.error_primer_apellido_edit)
+        segundo_edit_apellido_number_validator = InputsValidator(self.error_segundo_apellido_edit)
         self.lineEdit_ci.setValidator(ci_string_validator)
         self.lineEdit_nombre.setValidator(nombre_number_validator)
         self.lineEdit_primer_apellido.setValidator(primer_apellido_number_validator)
@@ -99,14 +97,19 @@ class Main(QMainWindow):
         self.lineEdit_primer_apellido_edit.setValidator(primer_edit_apellido_number_validator)
         self.lineEdit_segundo_apellido_edit.setValidator(segundo_edit_apellido_number_validator)
         self.listWidget.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.edit_button.clicked.connect(self.save_updated_client)
+        
+
+    
         
     def get_assets(self, file):
         return os.path.join(self.assets_path, file)
 
     def get_media(self, file):
-        media_path = os.path.join(self.assets_path, "assets/")
+        media_path = os.getcwd()
+        media_path = media_path.replace("\\", "/")
         return os.path.join(media_path, file)
-    
+
     def goto_home(self):
         self.states.views['main_view']=0
         self.switch_screen.setCurrentIndex(self.states.views['main_view'])
@@ -132,18 +135,30 @@ class Main(QMainWindow):
             filename, _ = QFileDialog.getOpenFileName(self, "Choose Image")
             if filename:
                 self.creating_client_picture = filename
-                pixmap = QPixmap(filename)
-                pixmap = pixmap.scaled(self.picture.size(), Qt.KeepAspectRatio)
-                self.picture.setPixmap(pixmap)
+                self.picture.setStyleSheet("""
+                                              border-image:""" f'url({filename})'
+                                              """0 0 0 0;
+                                             """)
+
+    def select_client_edit_picture(self, event):
+        if event.button() == Qt.LeftButton:
+            filename, _ = QFileDialog.getOpenFileName(self, "Choose Image")
+            if filename:
+                self.creating_client_edit_picture = filename
+                self.picture_edit.setStyleSheet("""
+                                              border-image:""" f'url({filename})'
+                                              """0 0 0 0;
+                                             """)
         
     
     def create_client(self):
         is_valid = True
 
-        if not self.lineEdit_ci.hasAcceptableInput() and not \
-            self.lineEdit_nombre.hasAcceptableInput() and not \
-            self.lineEdit_primer_apellido.hasAcceptableInput() and not \
-            self.lineEdit_segundo_apellido.hasAcceptableInput() and len(self.creating_client_picture) != 0:
+        if not self.lineEdit_ci_edit.hasAcceptableInput() and not \
+            self.lineEdit_nombre_edit.hasAcceptableInput() and not \
+            self.lineEdit_primer_apellido_edit.hasAcceptableInput() and not \
+            self.lineEdit_segundo_apellido_edit.hasAcceptableInput() and self.creating_client_picture == None or \
+            len(self.creating_client_picture) != 0:
             
             for client in self.states.data['client_list']:
                 if self.lineEdit_ci.text() == client['ci']:
@@ -166,25 +181,43 @@ class Main(QMainWindow):
                 self.error_ci.setText('Ya existe un cliente con ese carné de identidad')
                 self.error_ci.show()
 
-    def edit_client(self):
-        self.lineEdit_ci_edit.setText('123123123')
+    def set_client_values_to_edit(self, ci_value):
+        self.editing_client_ci = ci_value
+        client = Cliente.objects.get(ci=ci_value)
+        self.lineEdit_ci_edit.setText(client.ci)
+        self.lineEdit_nombre_edit.setText(client.nombre)
+        self.lineEdit_primer_apellido_edit.setText(client.primer_apellido)
+        self.lineEdit_segundo_apellido_edit.setText(client.segundo_apellido)
+        self.tipo_entrenamiento_edit.setCurrentText(client.tipo_entrenamiento)
+        self.picture_edit.clear()
+        picture_path = client.foto.url[1:]
+        picture_path = self.get_media(picture_path).replace("\\", "/")
+        self.picture_edit.setStyleSheet("""
+                                              border-image:""" f'url({picture_path})'
+                                              """0 0 0 0;
+                                             """)
+
+
+    def save_updated_client(self):
+        client = Cliente.objects.get(ci=self.editing_client_ci)
+        print(self.creating_client_edit_picture)
 
         if not self.lineEdit_ci_edit.hasAcceptableInput() and not \
             self.lineEdit_nombre_edit.hasAcceptableInput() and not \
             self.lineEdit_primer_apellido_edit.hasAcceptableInput() and not \
-            self.lineEdit_segundo_apellido_edit.hasAcceptableInput() and len(self.creating_client_picture) != 0:
-
-            with open(self.creating_client_picture, 'rb') as picture:
+            self.lineEdit_segundo_apellido_edit.hasAcceptableInput() and self.creating_client_edit_picture == None or \
+            len(self.creating_client_edit_picture) != 0:
+            with open(self.creating_client_edit_picture, 'rb') as picture:
                 uploaded_file = SimpleUploadedFile(picture.name, picture.read(), content_type='image/jpeg')
-                Cliente.objects.create(
-                    ci = self.lineEdit_ci_edit.text(),
-                    nombre = self.lineEdit_nombre_edit.text(),
-                    primer_apellido = self.lineEdit_primer_apellido_edit.text(),
-                    segundo_apellido = self.lineEdit_segundo_apellido_edit.text(),
-                    tipo_entrenamiento = self.tipo_entrenamiento_edit.currentText(),
-                    foto = uploaded_file,
-                )
+                client.ci = self.lineEdit_ci_edit.text()
+                client.nombre = self.lineEdit_nombre_edit.text()
+                client.primer_apellido = self.lineEdit_primer_apellido_edit.text()
+                client.segundo_apellido = self.lineEdit_segundo_apellido_edit.text()
+                client.tipo_entrenamiento = self.tipo_entrenamiento_edit.currentText()
+                client.foto = uploaded_file
+            client.save()
             self.goto_clients_list()
+            self.update_client_list()
 
     def delete_client(self, value):
         try:
@@ -198,9 +231,11 @@ class Main(QMainWindow):
             print(f'An error occurred: {err}')
 
 
-    def handle_view_changed(self, value):
-       self.states.views['clients_view'] = value
+    def handle_view_changed(self, index, ci_value):
+       self.states.views['clients_view'] = index
        self.clients_switch_screen.setCurrentIndex(self.states.views['clients_view'])
+       self.set_client_values_to_edit(ci_value)
+
 
     def update_client_list(self):
         self.listWidget.clear()
